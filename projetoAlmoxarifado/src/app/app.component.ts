612bf7e8682c3cd69334347d0b12d543e29e5953
murlock <mvinicius.avelino@hotmail.com>
2024-02-01 10:57:02 -0300

    configura submenus

--- projetoAlmoxarifado/src/app/app.component.ts
+++ projetoAlmoxarifado/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { PoMenuItem } from '@po-ui/ng-components';
 
@@ -8,13 +9,16 @@ import { PoMenuItem } from '@po-ui/ng-components';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  constructor ( public router: Router) {}
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) }
+    { label: 'Loging', shortLabel: 'log', icon: "po-icon-user", link:"/" },
+    { label: 'Home', shortLabel: 'h',icon: "po-icon-home", link:"/home" },
+    { label: 'Produtos', shortLabel: 'prd',icon: "po-icon-folder", link:"/productPage", subItems:[
+      { label: 'Estoque', shortLabel: 'Estq', icon: "po-icon-folder", link:"/storagePage"},
+      { label: 'Pedido de compra', shortLabel: 'Estq', icon: "po-icon-folder", link:"/storagePage"}
+    ]},
   ];
 
-  private onClick() {
-    alert('Clicked in menu item')
-  }
+  
 
 }
--- projetoAlmoxarifado/src/app/home-page/home-page.component.ts
+++ projetoAlmoxarifado/src/app/home-page/home-page.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { PoMenuItem } from '@po-ui/ng-components';
-
 @Component({
   selector: 'app-root',
   templateUrl: './home-page.component.html',
@@ -11,14 +9,6 @@ import { PoMenuItem } from '@po-ui/ng-components';
 export class HomePageComponent {
   constructor ( public router: Router) {}
 
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Loging', shortLabel: 'log', icon: "po-icon-user",link:"" },
-    { label: 'Home', shortLabel: 'h',icon: "po-icon-home",link:"home" },
-    { label: 'Produtos', shortLabel: 'prd',icon: "po-icon-folder",link:"productPage" },
-  ];
 
-  private onClick() {
-    alert('Clicked in menu item')
-  }
 
 }
